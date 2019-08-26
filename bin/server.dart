a840a0a7cd2eb67e891ef2d8e49b661578684a21
Luke Zhang <lz2504@columbia.edu>
2019-08-25 22:23:19 -0400

    Completed /_internal_/healthcheck

--- bin/server.dart
+++ bin/server.dart
@@ -5,13 +5,12 @@ Future main() async {
   var port = portEnv == null ? 4040 : int.parse(portEnv);
 
   var server = await HttpServer.bind(
-    InternetAddress.anyIPv4,
+    InternetAddress.anyIPv6,
     port,
   );
-  print('Listening on localhost:${server.port}');
+  print('Listening on:${server.port}');
 
   await for (HttpRequest request in server) {
-    print(request.toString());
     handleRequest(request);
     await request.response.close();
   }
@@ -21,29 +20,51 @@ void handleRequest(HttpRequest request){
   try{
     if(request.method == 'GET'){
       handleGet(request);
-    } else {
-      request.response
-          ..statusCode = HttpStatus.methodNotAllowed
-          ..write('Unsupported request: ${request.method}.')
-          ..close();
-    }
+    } else
+      sendMethodNotAllowed(request.response, request.method);
   } catch (e) {
     print('Exception in handleRequest: $e');
   }
-
-  print('Request handled');
 }
 
 void handleGet(HttpRequest request) {
-//  final guess = request.uri.queryParameters
+  List<String> segments = request.uri.pathSegments;
+
   final response = request.response;
-  response.statusCode = HttpStatus.ok;
-  response
-    ..statusCode = HttpStatus.ok
-    ..writeln('true')
-    ..close();
+
+  if(segments.isEmpty)
+    sendNotFound(response);
+  else{
+    if(segments.first == "generators"){
+
+    }
+    else if(segments.first == "_internal_"){
+      if(segments.length < 2 || segments[1] != "healthcheck")
+        sendNotFound(response);
+      else {
+        response
+          ..statusCode = HttpStatus.ok
+          ..close();
+      }
+    }
+    else
+      sendNotFound(response);
+  }
 }
 
 void handlePost(HttpRequest request){
 
+}
+
+void sendNotFound(HttpResponse response){
+  response
+    ..statusCode = HttpStatus.notFound
+    ..close();
+}
+
+void sendMethodNotAllowed(HttpResponse response, String method){
+  response
+    ..statusCode = HttpStatus.methodNotAllowed
+    ..write('Unsupported request: $method.')
+    ..close();
 }
